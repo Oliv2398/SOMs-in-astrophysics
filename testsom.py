50b8ea17d1fffc1056a22f0e646c9b5eb815316d
Olivier Roth <olivier_rt@yahoo.fr>
2020-06-01 12:54:15 +0200

    first try on SOMs with colors

---
+++ testsom.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from minisom import MiniSom
+
+colors = [[0., 0., 0.],
+      [0., 0., 1.],
+      [0., 0., 0.5],
+      [0.125, 0.529, 1.0],
+      [0.33, 0.4, 0.67],
+      [0.6, 0.5, 1.0],
+      [0., 1., 0.],
+      [1., 0., 0.],
+      [0., 1., 1.],
+      [1., 0., 1.],
+      [1., 1., 0.],
+      [1., 1., 1.],
+      [.33, .33, .33],
+      [.5, .5, .5],
+      [.66, .66, .66]
+      ]
+
+color_names = \
+    ['black', 'blue', 'darkblue', 'skyblue',
+     'greyblue', 'lilac', 'green', 'red',
+     'cyan', 'violet', 'yellow', 'white',
+     'darkgrey', 'mediumgrey', 'lightgrey']
+
+
+
+som = MiniSom(30, 30, 3, sigma=3.,
+              learning_rate=2.5,
+              neighborhood_function='gaussian')
+som.train_random(colors, 5000, verbose=True)
+
+plt.imshow(abs(som.get_weights()))
+plt.show()
