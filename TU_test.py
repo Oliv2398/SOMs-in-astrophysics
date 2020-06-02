e2da811823f899199ff72d50af441463fee57a5f
Olivier Roth <olivier_rt@yahoo.fr>
2020-06-02 15:30:54 +0200

    tests with TU catalog

---
+++ TU_test.py
@@ -0,0 +1,72 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import galsim
+from scipy.stats import skewnorm
+
+def TotRadius(bulge_radius, disk_radius, bt):
+    if bt == 0.:
+        radius = disk_radius
+    elif bt == 1.:
+        radius = bulge_radius
+    else:
+        if (bulge_radius < disk_radius):
+            radius = np.power((disk_radius*(1-bt)), 0.8) + \
+                np.power((bulge_radius * (bt)), 0.8)
+        else:
+            radius = np.power((disk_radius*(1-bt)), 2) + \
+                np.power((bulge_radius * (bt)), 2)
+    return radius
+
+def get_params(cat, i, pixel_scale):   #cat = TU cat, i = row of the cat
+    radiusBulge = cat.getFloat(i, 5)
+    radiusDisk = cat.getFloat(i, 8)*1.6783
+    bt = cat.getFloat(i, 4)
+    ell_B = cat.getFloat(i, 6)
+    ell_D = cat.getFloat(i, 9)
+    q = bt*ell_B + (1-bt)*ell_D
+    mag = cat.getFloat(i, 3)
+    PA = cat.getFloat(i, 7)
+
+    # sersic n param prop to bt
+    Nb = bt * 4 + 0.4
+    SSersic_n = skewnorm.rvs(6, Nb, 0.3, size=1)[0]
+    if SSersic_n < 0.1:
+        SSersic_n = np.random.normal(0.5, 0.1, 1)
+        if SSersic_n < 0.1:
+            SSersic_n = 0.1
+    if SSersic_n > 6:
+        SSersic_n = skewnorm.rvs(6, 5.5, 0.1, size=1)[0]
+        if SSersic_n > 6:
+            SSersic_n = 5.6
+
+    # Light radius in pixel
+    light_radius = TotRadius(radiusBulge, radiusDisk, bt) / pixel_scale
+    half_light_radius = float(light_radius) * np.sqrt(q)
+    return [mag, half_light_radius, q, SSersic_n, PA, bt]
+
+
+path_TU = "datas/TU_cat_field_0.list"
+
+# numpy load
+#TU_np = np.loadtxt(path_TU)
+#TUshape = TU_np.shape
+
+# galsim load
+TU = galsim.Catalog(path_TU)
+
+idx = 0
+Params=[]
+while True:
+    try:
+        para = get_params(TU, idx, 1)
+        Params.append(para)
+        idx+=1
+        print(idx, end='\r')
+    except :
+        print(idx, "lines")
+        break
+
+Params = np.concatenate(Params)
+Params = Params.T.reshape(idx,6)
+print(Params.shape)
+print(Params[0])
