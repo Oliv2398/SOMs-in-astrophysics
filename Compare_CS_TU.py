799a42cebffc709e6f70fda1d7aa1aab11355574
Olivier Roth <olivier_rt@yahoo.fr>
2020-07-03 19:07:34 +0200

    comparisons between COSMOS and TU

--- Compare_CS_TU.py
+++ Compare_CS_TU.py
@@ -1,7 +1,9 @@
+# Roth Olivier 07/2020
+# Catalog variable comparisons (with histograms)
+
 import numpy as np
 import matplotlib.pyplot as plt
 from astropy.io import fits
-from minisom import MiniSom
 
 
 path_TU = "datas/TU_created.fits"
@@ -31,16 +33,17 @@ def cut_hight_hlr(cat, lim): # delete problems
 
 data_CS = cut_hight_hlr(data_CS, 40) # cut hight values
 
-data_CS['sersicfit'][:,1] *= 0.03 # converting
+data_CS['sersicfit'][:,1] *= 0.03 # converting hlr
 
 
 
 def plot_compare(param_tu, param_cs, bins_tu, bins_cs, limits, *arg):
+
     fig, ax = plt.subplots(nrows=1, ncols=2, sharey=True, figsize=(8,6))
 
     #~~~~~~~~~~~~~~
     ax[0].set_title('True Universe catalog')
-    ax[0].hist(data_TU[param_tu], bins=bins_tu) # histogram
+    ax[0].hist(data_TU[param_tu], bins=bins_tu, density=1)
     ax[0].axvline(np.mean(data_TU[param_tu]),
         color='k', linestyle='--') # vertical line for the mean value
 
@@ -50,13 +53,14 @@ def plot_compare(param_tu, param_cs, bins_tu, bins_cs, limits, *arg):
     #~~~~~~~~~~~~~~
     ax[1].set_title('COSMOS catalog')
     if arg:# if a column has to be selected
-        ax[1].hist(data_CS[param_cs][:,arg], bins=bins_cs) # histogram
+        ax[1].hist(data_CS[param_cs][:,arg], bins=bins_cs, density=True)
         ax[1].axvline(np.mean(data_CS[param_cs][:,arg]),
             color='k', linestyle='--', label='moyenne') # vertical line for the mean value
     else:
-        ax[1].hist(data_CS[param_cs], bins=bins_cs) # histogram
+        ax[1].hist(data_CS[param_cs], bins=bins_cs, density=True)
         ax[1].axvline(np.mean(data_CS[param_cs]),
             color='k', linestyle='--', label='moyenne') # vertical line for the mean value
+
 
     ax[1].set_xlim(limits[1]) # x limits
     ax[1].set_xlabel(param_cs)
@@ -66,10 +70,11 @@ def plot_compare(param_tu, param_cs, bins_tu, bins_cs, limits, *arg):
     plt.legend()
     plt.show()
 
-plot_compare('mag', 'mag_auto', 300, 50, ((15,35), (15,35)))
 
-#plot_compare('half_light_radius', 'sersicfit', 1000, 10, ((-1,2),(-1,2)),1)
+#plot_compare('mag', 'mag_auto', 300, 50, ((15,35), (15,35)))
 
-#plot_compare('q', 'sersicfit', 200, 100, ((-1,2),(-1,2)),3)
+#plot_compare('half_light_radius', 'sersicfit', 5000, 1000, ((-1,2),(-1,2)),1)
 
-#plot_compare('SSersic_n', 'sersicfit', 200, 70, ((-2,8),(-2,8)),2)
+#plot_compare('q', 'sersicfit', 200, 100, ((0,1.2),(0,1.2)),3)
+
+plot_compare('SSersic_n', 'sersicfit', 200, 200, ((-.2,6.5),(-.2,6.5)),2)
