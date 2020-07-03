696af4692732d687ded8b43be2f0ebcaf08d7228
Olivier Roth <olivier_rt@yahoo.fr>
2020-07-03 19:11:40 +0200

    TU catalog to fits file

--- extract_TU.py
+++ extract_TU.py
@@ -1,7 +1,7 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import galsim
 from scipy.stats import skewnorm
+import galsim
+from astropy.table import Table
 
 def TotRadius(bulge_radius, disk_radius, bt):
     if bt == 0.:
@@ -30,6 +30,7 @@ def get_params(cat, i, pixel_scale):   #cat = TU cat, i = row of the cat
     # sersic n param prop to bt
     Nb = bt * 4 + 0.4
     SSersic_n = skewnorm.rvs(6, Nb, 0.3, size=1)[0]
+
     if SSersic_n < 0.1:
         SSersic_n = np.random.normal(0.5, 0.1, 1)
         if SSersic_n < 0.1:
@@ -45,28 +46,37 @@ def get_params(cat, i, pixel_scale):   #cat = TU cat, i = row of the cat
     return [mag, half_light_radius, q, SSersic_n, PA, bt]
 
 
-path_TU = "datas/TU_cat_field_0.list"
+def cut_hight_hlr(cat, lim): # delete hlr problems
+    idx = np.where(cat[:,1]>lim)[0]
+    print("element suppr :", idx.shape)
+    cat = np.delete(cat,idx)
+    return cat
 
-# numpy load
-#TU_np = np.loadtxt(path_TU)
-#TUshape = TU_np.shape
 
-# galsim load
-TU = galsim.Catalog(path_TU)
+def to_fits_file(): # transform list to fits TU catalog
+    path_TU = "datas/TU_cat_field_0.list"
 
-idx = 0
-Params=[]
-while True:
-    try:
-        para = get_params(TU, idx, 1)
-        Params.append(para)
-        idx+=1
-        print(idx, end='\r')
-    except :
-        print(idx, "lines")
-        break
+    TU = galsim.Catalog(path_TU)
 
-Params = np.concatenate(Params)
-Params = Params.T.reshape(idx,6)
-print(Params.shape)
-print(Params[0])
+    idx = 0
+    Params=[]
+    while True: # extracting lines one by one
+        try:
+            para = get_params(TU, idx, 1)
+            Params.append(para)
+            idx+=1
+            print(idx, end='\r')
+        except :
+            print(idx, "lines")
+            break
+
+    Params = np.concatenate(Params)
+    Params = Params.T.reshape(idx,6)
+
+    #Params = cut_hight_hlr(Params,1) # cut hlr
+
+    t = Table(Params, names=('mag', 'half_light_radius', 'q', 'SSersic_n', 'PA', 'bt'))
+    t.write('TU_created.fits', format='fits') # creating the fits file
+
+
+to_fits_file()
